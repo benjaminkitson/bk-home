da2996832dbf35d36b7329d4b6245a4b507f85f9
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-09-19 19:28:57 +0100

    change to black

--- src/app/(home)/components/TopSection.tsx
+++ src/app/(home)/components/TopSection.tsx
@@ -23,10 +23,10 @@ export const TopSection = () => {
         </div>
         <div className="my-4 flex w-36 justify-center">
           <a href="https://github.com/benjaminkitson" className="mr-6">
-            <BsGithub className=" fill-gray-300 text-3xl transition duration-500 hover:scale-90 hover:fill-blue-700" />
+            <BsGithub className=" fill-gray-300 text-3xl transition duration-500 hover:scale-90 hover:fill-black" />
           </a>
           <a href="https://uk.linkedin.com/in/benjamin-kitson">
-            <BsLinkedin className="fill-gray-300 text-3xl transition duration-500 hover:scale-90 hover:fill-blue-700" />
+            <BsLinkedin className="fill-gray-300 text-3xl transition duration-500 hover:scale-90 hover:fill-black" />
           </a>
         </div>
       </div>
