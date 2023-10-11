64c2092340b818ce0234aeea747beceeff93c2a3
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-11 13:54:58 +0100

    mobile tweak

--- src/app/(home)/components/TopSection.tsx
+++ src/app/(home)/components/TopSection.tsx
@@ -17,7 +17,7 @@ export const TopSection: React.FC<SectionProps> = ({ scrollRefs }) => {
         <Image
           src={me}
           alt="Benjamin Kitson"
-          className="m-2 h-1/4 w-56 rounded-3xl object-cover md:w-1/2 lg:h-1/2"
+          className="m-2 h-1/4 w-56 rounded-3xl object-cover lg:h-1/2"
         />
         <div className="my-10 flex flex-col items-center lg:ml-14">
           <div className="flex w-full flex-col justify-start px-4 pt-2">
@@ -27,7 +27,7 @@ export const TopSection: React.FC<SectionProps> = ({ scrollRefs }) => {
               love using both my creativity and my analytical skills to build
               apps that deliver real value to users.
             </P>
-            <P className="mb-10">
+            <P className="hidden md:block">
               When I&apos;m not working on Penfold&apos;s customer-facing mobile
               app, I&apos;m probably either building some random personal
               project {" ("}
@@ -38,7 +38,7 @@ export const TopSection: React.FC<SectionProps> = ({ scrollRefs }) => {
               or playing and writing music.{" "}
             </P>
           </div>
-          <div className="flex justify-center">
+          <div className="mt-10 flex justify-center">
             <a href="https://github.com/benjaminkitson" className="mr-6">
               <BsGithub className=" fill-gray-100 text-4xl transition duration-500 hover:scale-90 hover:fill-black" />
             </a>
