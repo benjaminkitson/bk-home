0a06c30d866608d1d7ddc08ce28e44fec0460b7f
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-12 10:35:27 +0100

    responsive typography, reinstate descriptions

--- src/components/Atoms/Typography.tsx
+++ src/components/Atoms/Typography.tsx
@@ -6,13 +6,17 @@ interface TypographyProps {
 }
 
 export const H1: React.FC<TypographyProps> = ({ children, className }) => {
-  return <h1 className={twMerge("text-5xl", className)}>{children}</h1>;
+  return (
+    <h1 className={twMerge("text-3xl md:text-5xl", className)}>{children}</h1>
+  );
 };
 
 export const H2: React.FC<TypographyProps> = ({ children, className }) => {
-  return <h2 className={twMerge("text-3xl", className)}>{children}</h2>;
+  return (
+    <h2 className={twMerge("text-xl md:text-3xl", className)}>{children}</h2>
+  );
 };
 
 export const P: React.FC<TypographyProps> = ({ children, className }) => {
-  return <p className={twMerge("text-lg", className)}>{children}</p>;
+  return <p className={twMerge("text-sm md:text-lg", className)}>{children}</p>;
 };
