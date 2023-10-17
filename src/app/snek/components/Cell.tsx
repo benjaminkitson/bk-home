440cf76ee6b9d781c77dc5c1e44977ac15002141
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-17 12:23:42 +0100

    more git hook test

--- src/app/snek/components/Cell.tsx
+++ src/app/snek/components/Cell.tsx
@@ -7,7 +7,7 @@ interface CellProps {
 
 const UnmemoizedCell: React.FC<CellProps> = ({ isSnek, isFood }) => {
   const bg = isSnek
-    ? "bg-blue-400"
+    ? "bg-blue-500"
     : isFood
     ? "bg-green-500"
     : "bg-white opacity-30";
