fb44c853281c9fb4b7e1316455def0250e312169
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-24 10:59:19 +0100

    fix annoying error

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,14 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  globals: {
-    // This is necessary because next.js forces { "jsx": "preserve" }, but ts-jest appears to require { "jsx": "react" }
-    "ts-jest": {
-      tsconfig: {
-        jsx: "react-jsx",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
       },
-    },
+    ],
   },
 };
